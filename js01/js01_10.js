96f2b7481ff07f4b89d0aa5f519e6f74670bc396
kjg <o_opop9999@naver.com>
2023-05-31 17:59:19 +0900

    조건문 배우기 -if

---
+++ js01/js01_10.js
@@ -0,0 +1,8 @@
+let result = true;
+if (result) console.log('result가 참 입니다.');
+if (!result)
+    console.log('실행되지 않습니다.');
+if (result) {
+    console.log('result의 결과');
+    console.log('>> 참 입니다.')
+}
