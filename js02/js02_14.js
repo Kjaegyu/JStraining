aa588689c9ca068eb1ad1c23aad17798b4099957
kjg <o_opop9999@naver.com>
2023-06-01 17:33:10 +0900

    반복문 배우기 - for in

---
+++ js02/js02_14.js
@@ -0,0 +1,7 @@
+let store = {snack: 1000, flower: 5000, beverage: 2000};
+
+for (let item in store) {
+    if (!store.hasOwnProperty(item)) continue;
+
+    console.log(item + '는 가격이' + store[item] + '입니다.')
+}
