7fb24e3164b328f9c58afc9ad7b522d1162585df
kjg <o_opop9999@naver.com>
2023-06-01 17:29:38 +0900

    조건문 배우기 - if, else if, else

---
+++ js02/js02_11.js
@@ -0,0 +1,10 @@
+let number =2;
+if (number == 1) {
+    console.log('number는 1 입니다');
+}   else if (number == 2) {
+    console.log('number는 2 입니다');
+} else if (number == 3) {
+    console.log('number는 3 입니다');
+} else {
+    console.log('number는 1,2,3 중 해당되는 것이 없습니다.');
+}
