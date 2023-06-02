dae90b3826c2a01ee2f5330e94566a95ef45e85e
kjg <o_opop9999@naver.com>
2023-06-02 18:21:37 +0900

    논리 연산자

---
+++ js03/js03_23.js
@@ -0,0 +1,17 @@
+console.log(true && true);
+console.log(true && false);
+console.log('문장' == '문장' && 5 == 5);
+console.log(5 == 5 && '다른 문장 1' == '다른 문장 2');
+
+// OR 논리 연산자||
+console.log(true || false);
+console.log(false || false);
+console.log('문장' == '문장' && 5 == 10);
+
+// NOT 논리 연산자 !
+console.log(!true);
+console.log(!false);
+console.log(!5);
+console.log(!'문장');
+console.log(!!5);
+console.log(!!'문장');
